903a931042ae419dc66aeeb3eea0a58f237765ac
= <=>
2025-02-17 21:39:28 +0900

    [update] 1. vpc & subnet

---
+++ var.tf
@@ -0,0 +1,45 @@
+variable "region" {
+  type    = string
+  default = "ap-northeast-2"
+}
+
+variable "cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "rocidr" {
+  type    = string
+  default = "0.0.0.0/0"
+}
+
+variable "name" {
+  type    = string
+  default = "cgv-prod"
+}
+
+variable "tag" {
+  type        = string
+  default     = "cgv-chill-guy"
+}
+
+variable "access_key" {
+  type        = string
+}
+
+variable "secret_key" {
+  type        = string
+}
+
+variable "db_username" {
+  type        = string
+}
+
+variable "db_password" {
+  type        = string
+}
+
+variable "db_name" {
+  type        = string
+}
+
